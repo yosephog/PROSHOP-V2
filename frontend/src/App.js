9b61de032c8210a54d28545c2acfcd41d3f16f54
yosephog <thorfry@yahoo.com>
2023-08-07 15:23:40 +0200

    components screens for the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 
 import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1>welcome to proshop</h1>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      
+      <Footer />
+    </>
+    
   )
 }
 
