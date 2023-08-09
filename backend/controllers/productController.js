a7f1fed15ef50ec8b4efa6eb881f8b0376900d5a
yosephog <thorfry@yahoo.com>
2023-08-09 09:34:46 +0200

    backend controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,21 @@
+import asyncHandler from "../middleware/asynchHandler.js";
+import Product from "../models/productModel.js";
+
+
+const getProducts = asyncHandler( async(req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+})
+
+const getProductById = asyncHandler( async(req, res) =>{
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        return res.json(product)
+    } else {
+        res.status(404)
+        throw new Error('Resource not found')
+    }
+})
+
+export {getProducts, getProductById}
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
-import products from './data/products.js'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 const port = process.env.PORT || 5000
+
+connectDB() // connect to mongoDB
 
 const app = express()
 
@@ -11,13 +15,9 @@ app.get('/', (req, res) => {
     res.send('API is running....')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((prod) => prod._id === req.params.id)
-    res.json(product)
-})
+app.use('/api/products', productRoutes)
 
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server running on ${port}`))
